45905912994cebdffd8574dd779b8b05d77ae459
mohammadhafiz <mohammadhafizamiruddin@gmail.com>
2015-09-23 14:38:50 +0800

    Feature api.

--- application/services/api.js
+++ application/services/api.js
@@ -6,7 +6,6 @@ application.factory('API', ['Settings',
         api.access_token    = '/api/access_token';
         api.session         = '/api/session';
         api.users           = '/api/users/:user_id';
-
         api.get = function(endpoint)
         {
             return Settings.host.concat(api[endpoint]);
