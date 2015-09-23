2d7c7244154b53557e70f281c485c50a249b1dc4
mohammadhafiz <mohammadhafizamiruddin@gmail.com>
2015-09-23 09:39:42 +0800

    Feature gulpfile and delete vendor.

--- application/controllers/login_controller.js
+++ application/controllers/login_controller.js
@@ -1,16 +1,8 @@
-application.controller('LoginController', ['$location', '$scope', 'Auth', 'isGuest',
-    function($location, $scope, Auth, isGuest)
+application.controller('LoginController', ['$scope', 'Auth', 'isGuest',
+    function($scope, Auth, isGuest)
     {
         $scope.credentials = {
             username: '',
             password: '',
-        };
-
-        $scope.login = function(credentials)
-        {
-            Auth.verify(credentials)
-                .then(function (session) {
-                    $location.path('/modules');
-                });
         };
     }]);
--- gulpfile.js
+++ gulpfile.js
@@ -1,221 +1,81 @@
-// Imports
-var gulp = require('gulp');
-var path = require('path');
+/*!
+ * gulp
+ * $ npm install gulp-ruby-sass gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename gulp-livereload gulp-cache del --save-dev
+ */
 
-// Plugin imports
-var concat = require('gulp-concat');
-var connect = require('gulp-connect');
-var htmlmin = require('gulp-htmlmin');
-var minifyCss = require('gulp-minify-css');
-var plumber = require('gulp-plumber');
-var sourcemaps = require('gulp-sourcemaps');
-var templateCache = require('gulp-angular-templatecache');
-var uglify = require('gulp-uglify');
+// Load plugins
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
 
-// Settings
-var settings = {
-    file: {
-        javascripts: 'application.min.js',
-        stylesheets: 'application.min.css',
-        templates: 'templates.min.js',
-        vendor: {
-            javascripts: 'vendor.min.js',
-            stylesheets: 'vendor.min.css',
-        },
-    },
-    folder: {
-        root: '.',
-        vendor: {
-            root: '.',
-            fonts: 'fonts',
-        },
-    },
-    htmls: [
-        'index.html',
-    ],
-    javascripts: [
-        'application/functions.js',
-        'application/application.js',
-        'application/settings.js',
-        'application/routes/**/*.js',
-        'application/translations/*.js',
-        'application/controllers/**/*.js',
-        'application/directives/*.js',
-        'application/filters/*.js',
-        'application/providers/*.js',
-        'application/services/*.js',
-    ],
-    stylesheets: [
-        'stylesheets/material-design-icons.css',
-        'stylesheets/application.css',
-    ],
-    templates: [
-        'templates/**/*.html',
-    ],
-    vendor: {
-        fonts: [
-            'bower_components/material-design-icons/iconfont/MaterialIcons-*',
-            'bower_components/roboto-fontface/fonts/*',
-        ],
-        javascripts: [
-            'bower_components/cryptojslib/rollups/aes.js',
-            'bower_components/angular/angular.js',
-            'bower_components/angular-animate/angular-animate.js',
-            'bower_components/angular-aria/angular-aria.js',
-            'bower_components/angular-material/angular-material.js',
-            'bower_components/angular-message-format/angular-message-format.js',
-            'bower_components/angular-messages/angular-messages.js',
-            'bower_components/angular-resource/angular-resource.js',
-            'bower_components/angular-route/angular-route.js',
-            'bower_components/angular-sanitize/angular-sanitize.js',
-            'bower_components/angular-translate/angular-translate.js',
-        ],
-        stylesheets: [
-            'bower_components/roboto-fontface/css/roboto-fontface.css',
-            'bower_components/angular-material/angular-material.css',
-        ],
-    },
-};
-
-// Tasks
-gulp.task('build', [
-    'htmls',
-    'javascripts',
-    'stylesheets',
-    'templates',
-]);
-
-gulp.task('default', [
-    'build',
-    'vendor'
-]);
-
-gulp.task('htmls', function()
-{
-    return gulp.src(settings.htmls)
-        .pipe(plumber())
-        .pipe(connect.reload());
+// Styles
+gulp.task('styles', function() {
+  return sass('src/styles/main.scss', { style: 'expanded' })
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(notify({ message: 'Styles task complete' }));
 });
 
-gulp.task('javascripts', function()
-{
-    return gulp.src(settings.javascripts)
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(concat(settings.file.javascripts))
-        .pipe(uglify())
-        .pipe(sourcemaps.write(settings.folder.root))
-        .pipe(gulp.dest(settings.folder.root))
-        .pipe(connect.reload());
+// Scripts
+gulp.task('scripts', function() {
+  return gulp.src('src/scripts/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(notify({ message: 'Scripts task complete' }));
 });
 
-gulp.task('server', function()
-{
-    var options = {
-        fallback: 'index.html',
-        host: '0.0.0.0',
-        livereload: true,
-        port: 8080
-    };
-
-    return connect.server(options);
+// Images
+gulp.task('images', function() {
+  return gulp.src('src/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe(notify({ message: 'Images task complete' }));
 });
 
-gulp.task('stylesheets', function()
-{
-    return gulp.src(settings.stylesheets)
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(concat(settings.file.stylesheets))
-        .pipe(minifyCss())
-        .pipe(sourcemaps.write(settings.folder.root))
-        .pipe(gulp.dest(settings.folder.root))
-        .pipe(connect.reload());
+// Clean
+gulp.task('clean', function(cb) {
+    del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], cb)
 });
 
-gulp.task('templates', function()
-{
-    var options = {
-        module: 'application.templates',
-        standalone: true,
-    };
-
-    return gulp.src(settings.templates)
-        .pipe(plumber())
-        .pipe(htmlmin())
-        .pipe(templateCache(settings.file.templates, options))
-        .pipe(uglify())
-        .pipe(gulp.dest(settings.folder.root))
-        .pipe(connect.reload());
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images');
 });
 
-gulp.task('vendor', [
-    'vendor:fonts',
-    'vendor:javascripts',
-    'vendor:stylesheets',
-]);
+// Watch
+gulp.task('watch', function() {
 
-gulp.task('vendor:fonts', function()
-{
-    return gulp.src(settings.vendor.fonts)
-        .pipe(plumber())
-        .pipe(gulp.dest(settings.folder.vendor.fonts));
-});
+  // Watch .scss files
+  gulp.watch('src/styles/**/*.scss', ['styles']);
 
-gulp.task('vendor:javascripts', function()
-{
-    return gulp.src(settings.vendor.javascripts)
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(concat(settings.file.vendor.javascripts))
-        .pipe(uglify())
-        .pipe(sourcemaps.write(settings.folder.vendor.root))
-        .pipe(gulp.dest(settings.folder.vendor.root));
-});
+  // Watch .js files
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
 
-gulp.task('vendor:stylesheets', function()
-{
-    return gulp.src(settings.vendor.stylesheets)
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(concat(settings.file.vendor.stylesheets))
-        .pipe(minifyCss())
-        .pipe(sourcemaps.write(settings.folder.vendor.root))
-        .pipe(gulp.dest(settings.folder.vendor.root));
-});
+  // Watch image files
+  gulp.watch('src/images/**/*', ['images']);
 
-gulp.task('watch', [
-    'server',
-    'watch:htmls',
-    'watch:javascripts',
-    'watch:stylesheets',
-    'watch:templates',
-]);
+  // Create LiveReload server
+  livereload.listen();
 
-gulp.task('watch:htmls', ['htmls'], function()
-{
-    return gulp.watch(settings.htmls, [
-        'htmls',
-    ]);
-});
+  // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', livereload.changed);
 
-gulp.task('watch:javascripts', ['javascripts'], function()
-{
-    return gulp.watch(settings.javascripts, [
-        'javascripts',
-    ]);
-});
-
-gulp.task('watch:stylesheets', ['stylesheets'], function()
-{
-    return gulp.watch(settings.stylesheets, [
-        'stylesheets',
-    ]);
-});
-
-gulp.task('watch:templates', ['templates'], function()
-{
-    return gulp.watch(settings.templates, [
-        'templates',
-    ]);
 });
