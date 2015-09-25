315d2ea5aad7441fa05c762ebecb5537509c507c
mohammadhafiz <mohammadhafizamiruddin@gmail.com>
2015-09-25 09:29:24 +0800

    Feature settings for index.

--- application/settings.js
+++ application/settings.js
@@ -1,20 +1,6 @@
-application.factory('Settings', ['$window', 'h', 'secret',
-    function($window, h, secret)
+application.factory('Settings',
+    function()
     {
         var settings = {};
-
-        settings.host           = $window.decrypt(h.ht, secret);
-        settings.client_id      = $window.decrypt(h.id, secret);
-        settings.client_secret  = $window.decrypt(h.st, secret);
-
-        settings.scopes = [
-            // Client credentials
-            'create_client_data',
-            'read_client_data',
-            'update_client_data',
-            'delete_client_data',
-
-        ];
-
         return settings;
-    }]);
+    });
