641d542d71369a906c473cac6e57c760a3a2ebae
mohammadhafiz <mohammadhafizamiruddin@gmail.com>
2015-09-26 09:59:20 +0800

    Fixed dashboard controller.

--- application/controllers/dashboard/dashboard_controller.js
+++ application/controllers/dashboard/dashboard_controller.js
@@ -28,9 +28,7 @@ application.controller('DashboardController', ['$interval', '$location', '$mdSid
         self.determinateValue = 30;
         self.determinateValue2 = 30;
         self.modes = [ ];
-        /**
-         * Turn off or on the 5 themed loaders
-         */
+
         self.toggleActivation = function() {
             if ( !self.activated ) self.modes = [ ];
             if (  self.activated ) {
@@ -39,18 +37,16 @@ application.controller('DashboardController', ['$interval', '$location', '$mdSid
               self.determinateValue2 = 30;
             }
         };
+        
         $interval(function() {
           self.determinateValue += 1;
           self.determinateValue2 += 1.5;
           if (self.determinateValue > 100) self.determinateValue = 30;
           if (self.determinateValue2 > 100) self.determinateValue2 = 30;
-            // Incrementally start animation the five (5) Indeterminate,
-            // themed progress circular bars
             if ( (j < 2) && !self.modes[j] && self.activated ) {
               self.modes[j] = (j==0) ? 'buffer' : 'query';
             }
             if ( counter++ % 4 == 0 ) j++;
-            // Show the indicator in the "Used within Containers" after 200ms delay
             if ( j == 2 ) self.contained = "indeterminate";
         }, 100, 0, true);
         $interval(function() {
