b913f943b9af79e5e4f0f3d72c79679e8da9b814
mohammadhafiz <mohammadhafizamiruddin@gmail.com>
2015-09-26 08:51:08 +0800

    Feature form content.

--- application/controllers/forms/forms_controller.js
+++ application/controllers/forms/forms_controller.js
@@ -21,4 +21,18 @@ application.controller('FormsController', ['$location', '$mdSidenav', '$scope', 
         {
             $mdSidenav('menu').open();
         };
+        
+        $scope.user = function()
+        {
+          title: 'Developer',
+          email: 'ipsum@lorem.com',
+          firstName: '',
+          lastName: '' ,
+          company: 'Google' ,
+          address: '1600 Amphitheatre Pkwy' ,
+          city: 'Mountain View' ,
+          state: 'CA' ,
+          biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
+          postalCode : '94043'
+        };
     }]);
