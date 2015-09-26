f0f0df9568eb8ea28eb8f05fb0b5867be2d3839b
mohammadhafiz <mohammadhafizamiruddin@gmail.com>
2015-09-26 10:45:53 +0800

    Feature datepicker.

--- application/controllers/ui_element/ui_element_controller.js
+++ application/controllers/ui_element/ui_element_controller.js
@@ -21,4 +21,40 @@ application.controller('UiElementController', ['$location', '$mdSidenav', '$scop
         {
             $mdSidenav('menu').open();
         };
+
+        $scope.items = [1,2,3,4,5];
+        
+        $scope.selected = [];
+        
+        $scope.toggle = function (item, list) 
+        {
+            var idx = list.indexOf(item);
+            if (idx > -1) list.splice(idx, 1);
+            else list.push(item);
+        };
+        
+        $scope.exists = function (item, list) 
+        
+        {
+        return list.indexOf(item) > -1;
+        };
+        
+        $scope.myDate = new Date();
+        
+        $scope.minDate = new Date(
+        
+        $scope.myDate.getFullYear(),
+        
+        $scope.myDate.getMonth() - 2,
+        
+        $scope.myDate.getDate());
+        
+        $scope.maxDate = new Date(
+        
+        $scope.myDate.getFullYear(),
+        
+        $scope.myDate.getMonth() + 2,
+        
+        $scope.myDate.getDate());   
     }]);
+    
