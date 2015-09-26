95671d32cfb5ee29c0ef4eff6aacded8a3b6e2c1
mohammadhafiz <mohammadhafizamiruddin@gmail.com>
2015-09-26 10:54:04 +0800

    Feature slider.

--- application/controllers/ui_element/ui_element_controller.js
+++ application/controllers/ui_element/ui_element_controller.js
@@ -56,5 +56,16 @@ application.controller('UiElementController', ['$location', '$mdSidenav', '$scop
         $scope.myDate.getMonth() + 2,
         
         $scope.myDate.getDate());   
+        
+        $scope.color = {
+            red: Math.floor(Math.random() * 255),
+            green: Math.floor(Math.random() * 255),
+            blue: Math.floor(Math.random() * 255)
+          };
+          $scope.rating1 = 3;
+          $scope.rating2 = 2;
+          $scope.rating3 = 4;
+          $scope.disabled1 = 0;
+          $scope.disabled2 = 70;
     }]);
     
