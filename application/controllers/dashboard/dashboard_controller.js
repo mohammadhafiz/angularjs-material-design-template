4de92a937c714c5790cde8c735b80d301235a833
mohammadhafiz <mohammadhafizamiruddin@gmail.com>
2015-09-26 08:38:15 +0800

    Feature dashboard content.

--- application/controllers/dashboard/dashboard_controller.js
+++ application/controllers/dashboard/dashboard_controller.js
@@ -1,5 +1,5 @@
-application.controller('DashboardController', ['$location', '$mdSidenav', '$scope', '$translate',
-    function($location, $mdSidenav, $scope, $translate)
+application.controller('DashboardController', ['$interval', '$location', '$mdSidenav', '$scope', '$translate', 
+    function($interval, $location, $mdSidenav, $scope, $translate)
     {
         $scope.theme = 'cyan';
 
@@ -21,4 +21,39 @@ application.controller('DashboardController', ['$location', '$mdSidenav', '$scop
         {
             $mdSidenav('menu').open();
         };
-    }]);
+
+        var self = this, j= 0, counter = 0;
+        self.mode = 'query';
+        self.activated = true;
+        self.determinateValue = 30;
+        self.determinateValue2 = 30;
+        self.modes = [ ];
+        /**
+         * Turn off or on the 5 themed loaders
+         */
+        self.toggleActivation = function() {
+            if ( !self.activated ) self.modes = [ ];
+            if (  self.activated ) {
+              j = counter = 0;
+              self.determinateValue = 30;
+              self.determinateValue2 = 30;
+            }
+        };
+        $interval(function() {
+          self.determinateValue += 1;
+          self.determinateValue2 += 1.5;
+          if (self.determinateValue > 100) self.determinateValue = 30;
+          if (self.determinateValue2 > 100) self.determinateValue2 = 30;
+            // Incrementally start animation the five (5) Indeterminate,
+            // themed progress circular bars
+            if ( (j < 2) && !self.modes[j] && self.activated ) {
+              self.modes[j] = (j==0) ? 'buffer' : 'query';
+            }
+            if ( counter++ % 4 == 0 ) j++;
+            // Show the indicator in the "Used within Containers" after 200ms delay
+            if ( j == 2 ) self.contained = "indeterminate";
+        }, 100, 0, true);
+        $interval(function() {
+          self.mode = (self.mode == 'query' ? 'determinate' : 'query');
+        }, 7200, 0, true);
+      }]);
