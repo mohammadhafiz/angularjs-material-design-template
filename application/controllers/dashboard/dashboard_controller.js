d3ba080b359d681e1931ab1ae61d53332b63c5d7
mohammadhafiz <mohammadhafizamiruddin@gmail.com>
2015-09-30 14:07:48 +0800

    Feature todo.

--- application/controllers/dashboard/dashboard_controller.js
+++ application/controllers/dashboard/dashboard_controller.js
@@ -21,35 +21,19 @@ application.controller('DashboardController', ['$interval', '$mdSidenav', '$scop
         {
             $scope.query.$ = '';
         };
-
-        var self = this, j= 0, counter = 0;
-        self.mode = 'query';
-        self.activated = true;
-        self.determinateValue = 30;
-        self.determinateValue2 = 30;
-        self.modes = [ ];
-
-        self.toggleActivation = function() {
-            if ( !self.activated ) self.modes = [ ];
-            if (  self.activated ) {
-              j = counter = 0;
-              self.determinateValue = 30;
-              self.determinateValue2 = 30;
-            }
-        };
         
-        $interval(function() {
-          self.determinateValue += 1;
-          self.determinateValue2 += 1.5;
-          if (self.determinateValue > 100) self.determinateValue = 30;
-          if (self.determinateValue2 > 100) self.determinateValue2 = 30;
-            if ( (j < 2) && !self.modes[j] && self.activated ) {
-              self.modes[j] = (j==0) ? 'buffer' : 'query';
-            }
-            if ( counter++ % 4 == 0 ) j++;
-            if ( j == 2 ) self.contained = "indeterminate";
-        }, 100, 0, true);
-        $interval(function() {
-          self.mode = (self.mode == 'query' ? 'determinate' : 'query');
-        }, 7200, 0, true);
+        $scope.todoList = [{todoText:'Clean House', done:false}];
+
+        $scope.todoAdd = function() {
+            $scope.todoList.push({todoText:$scope.todoInput, done:false});
+            $scope.todoInput = "";
+        };
+
+        $scope.remove = function() {
+            var oldList = $scope.todoList;
+            $scope.todoList = [];
+            angular.forEach(oldList, function(x) {
+                if (!x.done) $scope.todoList.push(x);
+            });
+        };
 }]);
